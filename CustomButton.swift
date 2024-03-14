8414efb5eb7ba80dad4734bfb66d9a6765489c1e
coolat34 <coolat34@gmail.com>
2024-03-14 13:51:51 +0000

    Create CustomButton.swift

---
+++ CustomButton.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct CustomButton: View {
+    let label: String
+    let width: Int
+    let isDisabled: Bool
+
+    init(label: String, width: Int = 300, isDisabled: Bool = false) {
+      self.label = label
+      self.width = width
+      self.isDisabled = isDisabled
+    }
+
+    var body: some View {
+        Text("\(label)").fontWeight(.heavy)
+        .foregroundColor(Color.black)
+        .frame(width: CGFloat(width), height: 48, alignment: .center)
+        .background(isDisabled ? Color.yellow : Color.teal)
+        .cornerRadius(16, antialiased: true)
+        .padding()
+        .animation(.easeInOut, value: isDisabled)
+    }
+  }
+
+  struct CustomButton_Previews: PreviewProvider {
+    static var previews: some View {
+      CustomButton(label: "This is the label", isDisabled: true)
+    }
+  }
