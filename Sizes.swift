390022292dece8057150487cdfad4f114fbef990
coolat34 <coolat34@gmail.com>
2024-03-14 13:51:08 +0000

    Create Sizes.swift

---
+++ Sizes.swift
@@ -0,0 +1,18 @@
+//
+//  Sizes.swift
+//  BoxCalc
+//
+//  Created by Chris Milne on 23/02/2024.
+//
+
+import SwiftUI
+
+class Sizes: ObservableObject {
+
+    @Published var length:Int64 = 0
+    @Published var width:Int64 = 0
+    @Published var height:Int64 = 0
+   
+}
+
+
